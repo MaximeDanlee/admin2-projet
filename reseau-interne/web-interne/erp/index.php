09a9b10b1020922ad484945d61d882242318c41c
MaximeDanlee <max@interplanet.be>
2021-04-02 07:38:51 -0700

    add mysql and database

--- reseau-interne/web-interne/erp/index.php
+++ reseau-interne/web-interne/erp/index.php
@@ -1,8 +1,30 @@
-<html>
- <head>
-  <title>Test PHP</title>
- </head>
- <body>
- <?php echo '<p>Bonjour le monde</p>'; ?>
- </body>
-</html>
+<?php
+$user = "example_user";
+$password = "password";
+$database = "example_database";
+$table = "todo_list";
+
+
+echo '<html>';
+echo '<head>';
+echo '<title>Test PHP</title>';
+echo '</head>';
+echo '<body>';
+echo '<p>Bonjour le monde</p>';
+
+ try {
+    $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    echo "<h2>TODO</h2><ol>";
+    foreach($db->query("SELECT content FROM $table") as $row) {
+      echo "<li>" . $row['content'] . "</li>";
+    }
+    echo "</ol>";
+  } catch (PDOException $e) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      die();
+  }
+
+ echo '</body>';
+ echo '</html>';
+
+ ?>
