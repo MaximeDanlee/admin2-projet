29390a447a792a21309d8dd3be2af10a0f9c8424
Miguel-Antoons <miguel.antoons@gmail.com>
2021-04-14 11:35:47 +0200

    troubleshooting dans le fichier shell

--- dns_externe/dnssec_key.sh
+++ dns_externe/dnssec_key.sh
@@ -10,6 +10,4 @@ done
 
 dnssec-signzone -A -3 $(head -c 1000 /dev/random | sha1sum | cut -b 1-16) -N INCREMENT -o tl2-2.ephec-ti.be -t db.tl2-2.ephec-ti.be
 
-named -g
-
 # https://www.digitalocean.com/community/tutorials/how-to-setup-dnssec-on-an-authoritative-bind-dns-server-2
